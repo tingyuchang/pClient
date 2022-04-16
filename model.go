eec06135f7e21703c6404caa08af8f12876ebd79
tingyuchang <tingyugenius@gmail.com>
2022-04-16 21:24:31 +0800

    Add: model structure

---
+++ model.go
@@ -0,0 +1,13 @@
+package pClient
+
+type User struct {
+	ID       int
+	Username string
+}
+
+type Userdata struct {
+	ID          int
+	Name        string
+	Surname     string
+	Description string
+}
